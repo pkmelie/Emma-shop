b742b68b0dee966cf5b471c4390dae7ab92e50b3
V3clix <elielululeo@gmail.com>
2026-05-04 12:50:19 +0200

    up

--- js/notifications.js
+++ js/notifications.js
@@ -27,19 +27,6 @@ export async function initNotifications(containerSelector, { isAdmin = false } =
   const wrapper = document.createElement('div');
   wrapper.className = 'notif-widget';
   wrapper.innerHTML = `
-    <button class="notif-bell" aria-label="Notifications" aria-expanded="false">
-      🔔
-      <span class="notif-badge" style="display:none">0</span>
-    </button>
-    <div class="notif-dropdown" hidden>
-      <div class="notif-header">
-        <span class="notif-title">Notifications</span>
-        <button class="notif-mark-all">Tout lire</button>
-      </div>
-      <ul class="notif-list">
-        <li class="notif-empty">Chargement…</li>
-      </ul>
-    </div>
   `;
 
   container.appendChild(wrapper);
